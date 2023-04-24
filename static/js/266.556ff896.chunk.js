6822191fb8e9d9dcec3fec4eb8d71b9c3f6fef1c
KornieievaAnna <109692085+KornieievaAnna@users.noreply.github.com>
2023-04-24 19:14:38 +0000

    Deploying to gh-pages from @ KornieievaAnna/tweets@57dc4849fa6d6cc66431b15cae931dafaf80fe54 🚀

---
+++ static/js/266.556ff896.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktweets=self.webpackChunktweets||[]).push([[266],{266:function(e,t,n){n.r(t);var s=n(184);t.default=function(){return(0,s.jsx)("div",{children:"Hellow"})}}}]);
+//# sourceMappingURL=266.556ff896.chunk.js.map
